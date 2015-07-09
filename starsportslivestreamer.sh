df6901f04e639f288beaba70140072d0bdd296bf
biezom <bolivebiezom@free.fr>
2015-07-10 01:10:45 +0200

    Update starsportslivestreamer.sh

--- starsportslivestreamer.sh
+++ starsportslivestreamer.sh
@@ -3,7 +3,7 @@ echo "paste the link"
 read link
 folder=$PWD/videos/
 livestreamer=""
-php starsportslivestreamer.php "$link" 
+php starsportslivestreamer.php "$link" "$livestreamer"
 echo "write quality (example write 720p)"
 read quality
 php starsportslivestreamer.php "$link" "$quality" "$folder" "$livestreamer"
