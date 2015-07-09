8d7a0e9adfdf1e5f03192dd92e85d8fb7090094f
biezom <bolivebiezom@free.fr>
2015-07-10 01:09:14 +0200

    Update starsportslivestreamer.bat

--- starsportslivestreamer.bat
+++ starsportslivestreamer.bat
@@ -2,7 +2,7 @@
 call set /p link=paste the link:
 call set folder="%~dp0\videos\\"
 call set livestreamer="%~dp0\tools\livestreamer\\"
-call "%~dp0\tools\php5.4\php.exe" starsportslivestreamer.php "%%link%%"
+call "%~dp0\tools\php5.4\php.exe" starsportslivestreamer.php "%%link%%" "%%livestreamer%%"
 call set /p quality=write quality (example write 720p):
 call "%~dp0\tools\php5.4\php.exe" starsportslivestreamer.php "%%link%%" "%%quality%%" "%%folder%%" "%%livestreamer%%"
 pause
